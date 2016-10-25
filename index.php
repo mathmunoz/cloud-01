88891893d071cd63ef652db0233ea6693682dbf8
Matias <math.ivan.munoz@gmail.com>
2016-10-25 12:10:26 -0300

    modificacion

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
-<?php
-	echo "Hola mundo"
-?>
+<DOCTYPE/>
+
+<html>
+	<head>
+		<title>cloud-01</title>
+	</head>
+	<body>
+		<h1> <?php echo "Hola Mundo"; ?> </h1>
+		<br>
+		<?php echo "<h3>Como estas?</h3>"; ?>
+	</body>
+</html>
